524435e16f0586ff3edbbbc1218b9d185ece1815
deder01 <david.eder@tufts.edu>
2012-12-04 16:46:52 -0500

    data models set up, south is working, 2 test cases

--- games/models.py
+++ games/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class ShanghiGame(models.Model):
-  n =  models.IntegerField()
+  name = models.CharField(max_length=100)
+  num_players = models.IntegerField(default=2)
 
 class ShanghiPlayer(models.Model):
-  headline = models.CharField(max_length=100)
-  game = models.ForeignKey(ShanghiGame)
+  game = models.ForeignKey(ShanghiGame, related_name="players")
+  player = models.ForeignKey(User, related_name="shangi_games")
+  ten = models.IntegerField(default=0)
+  eleven = models.IntegerField(default=0)
+  tweleve = models.IntegerField(default=0)
+  thirteen = models.IntegerField(default=0)
+  fourteen = models.IntegerField(default=0)
+  fifteen = models.IntegerField(default=0)
+  sixteen = models.IntegerField(default=0)
+  seventeen = models.IntegerField(default=0)
+  eighteen = models.IntegerField(default=0)
+  nineteen = models.IntegerField(default=0)
+  twenty = models.IntegerField(default=0)
+  bull = models.IntegerField(default=0)
