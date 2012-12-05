a59df888363a270d074a884588433924f3b05b40
deder01 <david.eder@tufts.edu>
2012-12-05 15:44:28 -0500

    testing rendering

--- games/views.py
+++ games/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from models import *
+
+def hello(request):
+  return HttpResponse("Hello World")
+
+def getPlayers(request):
+  player_list = ShanghiPlayers.objects.all()
+  return render_to_response('index.html', {'player_list': player_list})
