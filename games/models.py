3b94ba9d7b1c8f113a21ddee2bda4b07880a7ba2
deder01 <david.eder@tufts.edu>
2012-12-06 10:50:58 -0500

    synch with laptop

--- games/models.py
+++ games/models.py
@@ -3,21 +3,18 @@ from django.contrib.auth.models import User
 
 class ShanghiGame(models.Model):
   name = models.CharField(max_length=100)
+  done = models.BooleanField(default="false")
   num_players = models.IntegerField(default=2)
+  current_round = models.IntegerField(default=10)
+  current_player = models.IntegerField(default=2)
 
 class ShanghiPlayer(models.Model):
   player_num = models.IntegerField(default=1)
   game = models.ForeignKey(ShanghiGame, related_name="players")
   player = models.ForeignKey(User, related_name="shangi_games")
-  ten = models.IntegerField(default=0)
-  eleven = models.IntegerField(default=0)
-  tweleve = models.IntegerField(default=0)
-  thirteen = models.IntegerField(default=0)
-  fourteen = models.IntegerField(default=0)
-  fifteen = models.IntegerField(default=0)
-  sixteen = models.IntegerField(default=0)
-  seventeen = models.IntegerField(default=0)
-  eighteen = models.IntegerField(default=0)
-  nineteen = models.IntegerField(default=0)
-  twenty = models.IntegerField(default=0)
-  bull = models.IntegerField(default=0)
+
+class ShanghiRound(models.Model):
+  round_number = models.IntegerField()
+  number_of_darts = models.IntegerField()
+  number_of_hits = models.IntegerField()
+  shanghiplayer = models.ForeignKey(ShanghiPlayer, related_name="rounds")
