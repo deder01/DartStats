978444911e02c318f286a860664c1c879428286f
deder01 <david.eder@tufts.edu>
2012-12-11 22:30:33 -0500

    made project path name

--- darts/settings.py
+++ darts/settings.py
@@ -117,11 +117,10 @@ ROOT_URLCONF = 'darts.urls'
 WSGI_APPLICATION = 'darts.wsgi.application'
 
 TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT_PATH, "templates"),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/deder/darts/DartStats/templates',
-    '/app/templates',
 )
 
 INSTALLED_APPS = (
