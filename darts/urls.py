a0bea10d08b3ad013481d772c4a21ef99c0da162
deder01 <david.eder@tufts.edu>
2012-12-11 22:49:48 -0500

    getting css to work on heroku

--- darts/urls.py
+++ darts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from games.views import *
-from django.conf 
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
